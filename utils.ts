8abc4dbb647ba4fd3206a3577abf83d42421c280
Jon Callahan <j@joncallahan.com>
2023-04-30 15:30:29 -0500

    feat: remove extra text before sending to openai

--- utils.ts
+++ utils.ts
@@ -2,5 +2,7 @@ export function cleanText(text: string) {
   // Strip all newlines
   return text.replace(/\n/g, " ")
     // Combine all multiple spaces into one
-    .replace(/ +/g, " ");
+    .replace(/ +/g, " ")
+    // Only pass the transaction alert
+    .split("You are receiving this alert because")[0];
 }
