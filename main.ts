999418edd474ca422d44c603f4d23c5c8c2fcf10
Jon Callahan <j@joncallahan.com>
2024-04-28 19:52:53 -0500

    feat: use tool calling via instructor

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 import { Hono } from "hono";
-import { z } from "zod";
+import { z } from "npm:zod@3.23.4";
 import "https://deno.land/std@0.180.0/dotenv/load.ts";
-import Anthropic from "npm:@anthropic-ai/sdk@0.19.0";
+import Instructor from "npm:@instructor-ai/instructor@1.2.1";
+import OpenAI from "npm:openai@4.38.5";
 import { nanoid } from "nanoid";
 import { trytm } from "trytm";
 import { cleanText } from "./utils.ts";
 import { htmlToText } from "./dom-parse.ts";
 import { sendNotification } from "./ntfy.ts";
 
-const ANTHROPIC_API_KEY = z.string().parse(Deno.env.get("ANTHROPIC_API_KEY"));
+const OPENAI_API_KEY = z.string().parse(Deno.env.get("OPENAI_API_KEY"));
 const AIRTABLE_API_KEY = z.string().parse(Deno.env.get("AIRTABLE_API_KEY"));
 export const AIRTABLE_BASE_ID = z.string().parse(
   Deno.env.get("AIRTABLE_BASE_ID"),
@@ -17,8 +18,14 @@ export const AIRTABLE_TABLE_NAME = z.string().parse(
   Deno.env.get("AIRTABLE_TABLE_NAME"),
 );
 
-const anthropic = new Anthropic({
-  apiKey: ANTHROPIC_API_KEY,
+const oai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
+
+const client = Instructor({
+  client: oai,
+  mode: "TOOLS",
+  debug: true,
 });
 
 const app = new Hono();
@@ -58,47 +65,91 @@ type PostmarkWebhookPayload = {
 //   "merchant" should be enriched to the common, well-known merchant name without store specific, location, or point-of-sale provider info, formatted for legibility. If the merchant is part of a restaurant group, extract the specific restaurant name instead of the group name.
 //   "category" should categorize the "merchant" into a budget category. Reply with JSON only.`;
 
-const prompt = `
-Here are the details of a credit card transaction:
+// const prompt = `
+// Here are the details of a credit card transaction:
 
+// <transaction>
+// {TRANSACTION}
+// </transaction>
+
+// Please format this transaction as a JSON object with the following fields and formats:
+
+// - "date" should be in the format YYYY-MM-DD
+// - "time" should be in the format HH:MM AM/PM TIMEZONE
+// - "amount" should be in the format $X.XX
+// - "account" should be the name and last 4 digits of the account number in parentheses
+// - "merchant_raw" should be the exact merchant name as it appears in the transaction details above
+// - "merchant" should be the common, well-known name for this merchant, without any store numbers,
+// locations, or point-of-sale provider information. Format it for maximum legibility. If the merchant
+// is part of a restaurant group, extract the specific restaurant name rather than using the group
+// name.
+// - "category" can ONLY be one of the following values: "Auto", "Food & Dining", "Pet", "Travel",
+// "Home", "Utilities", "Gifts/Donation", "Shopping", "Baby/Kid", "Taxes", or "Other". If the merchant
+// does not clearly fit into any of those categories, use "Other". Note that grocery store purchases
+// should be categorized as "Food & Dining".
+
+// Output the JSON object with no other explanatory text. Here is an example of the desired output
+// format:
+
+// <example>
+// {"date": "2021-12-31", "time": "4:35 PM ET", "amount": "$1.00", "account": "Checking (1234)",
+// "merchant_raw": "SQ* SWEET GREEN CHICAGO", "merchant": "Sweet Green", "category": "Food & Dining"}
+// </example>
+// `;
+
+const prompt = `
+You are an expert in parsing credit card transactions and categorizing them.
 <transaction>
 {TRANSACTION}
 </transaction>
-
-Please format this transaction as a JSON object with the following fields and formats:
-
-- "date" should be in the format YYYY-MM-DD
-- "time" should be in the format HH:MM AM/PM TIMEZONE
-- "amount" should be in the format $X.XX
-- "account" should be the name and last 4 digits of the account number in parentheses
-- "merchant_raw" should be the exact merchant name as it appears in the transaction details above
-- "merchant" should be the common, well-known name for this merchant, without any store numbers,
-locations, or point-of-sale provider information. Format it for maximum legibility. If the merchant
-is part of a restaurant group, extract the specific restaurant name rather than using the group
-name.
-- "category" can ONLY be one of the following values: "Auto", "Food & Dining", "Pet", "Travel",
-"Home", "Utilities", "Gifts/Donation", "Shopping", "Baby/Kid", "Taxes", or "Other". If the merchant
-does not clearly fit into any of those categories, use "Other". Note that grocery store purchases
-should be categorized as "Food & Dining".
-
-Output the JSON object with no other explanatory text. Here is an example of the desired output
-format:
 
 <example>
 {"date": "2021-12-31", "time": "4:35 PM ET", "amount": "$1.00", "account": "Checking (1234)",
 "merchant_raw": "SQ* SWEET GREEN CHICAGO", "merchant": "Sweet Green", "category": "Food & Dining"}
 </example>
+
+<example>
+{"date": "2021-12-31", "time": "4:35 PM ET", "amount": "$1.00", "account": "Checking (1234)",
+"merchant_raw": "TST* LEA FRENCH STRE", "merchant": "Lea French Street Food", "category": "Food & Dining"}
+</example>
 `;
 
 const schema = z.object({
-  date: z.string(),
-  time: z.string(),
-  amount: z.string(),
-  account: z.string(),
-  merchant: z.string(),
-  merchant_raw: z.string(),
-  category: z.string(),
-});
+  date: z.string().describe("Date of the transaction in YYYY-MM-DD format"),
+  time: z.string().describe(
+    "Time of the transaction in HH:MM AM/PM TIMEZONE format",
+  ),
+  amount: z.string().describe("Amount of the transaction in $X.XX format"),
+  account: z.string().describe(
+    "Name and last 4 digits of the account number in parentheses",
+  ),
+  merchant_raw: z.string().describe(
+    "Exact merchant name as it appears in the transaction details",
+  ),
+  merchant: z.string().describe(
+    "Common, well-known name for this merchant, without any store numbers, locations, or point-of-sale provider information. Format it for maximum legibility. If the merchant is part of a restaurant group, extract the specific restaurant name rather than using the group name. If the name is cut off, provide the full name.",
+  ),
+  category: z.enum([
+    "Auto",
+    "Food & Dining",
+    "Pet",
+    "Travel",
+    "Home",
+    "Utilities",
+    "Gifts/Donation",
+    "Shopping",
+    "Baby/Kid",
+    "Taxes",
+    "Other",
+  ])
+    .describe(
+      `Category of the transaction. If the merchant
+    does not clearly fit into any of those categories, use "Other". Note that grocery store purchases
+    should be categorized as "Food & Dining".`,
+    ),
+}).describe(
+  "Credit card transaction alert extracted from an email",
+);
 
 app.get("/", () => {
   return new Response("YO Deno!", {
@@ -139,37 +190,33 @@ app.post("/inbound-email", async (c) => {
     " calling LLM with payload:",
   );
   console.log(txnAlert);
+  const content = prompt.replace("{TRANSACTION}", txnAlert);
+  console.log("Calling LLM with content:", content);
 
   console.time("llm_completion");
-  const chat_completion = await anthropic.messages.create({
-    model: "claude-3-sonnet-20240229",
-    messages: [
-      {
+  const [_transaction, transactionError] = await trytm(
+    client.chat.completions.create({
+      messages: [{
         role: "user",
-        content: prompt.replace("{TRANSACTION}", txnAlert),
+        content,
+      }],
+      model: "gpt-4-turbo",
+      max_tokens: 256,
+      top_p: 0.5,
+      max_retries: 3,
+      response_model: {
+        schema,
+        name: "Transaction",
       },
-    ],
-    temperature: 0.1,
-    max_tokens: 256,
-  });
+    }),
+  );
   console.timeEnd("llm_completion");
 
-  console.log(
-    "Got chat completion",
-    chat_completion.content.at(-1)?.text,
-  );
-
-  const completion = JSON.parse(chat_completion.content.at(-1)?.text || "{}");
-  const result = schema.safeParse(completion);
-
-  if (!result.success) {
-    console.log("Completion does not match schema");
-    console.log(completion);
-    console.error(result.error);
-    return c.text("NOT OK", 500);
+  if (transactionError) {
+    console.error(transactionError);
+    return c.text("Error processing email", 500);
   }
-
-  console.log(result.data);
+  const transaction = await _transaction;
 
   const airtable_res = await fetch(
     `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`,
@@ -184,13 +231,13 @@ app.post("/inbound-email", async (c) => {
           {
             fields: {
               ID: nanoid(),
-              Date: result.data.date,
-              Time: result.data.time,
-              Amount: result.data.amount,
-              Account: result.data.account,
-              Merchant: result.data.merchant,
-              Category: result.data.category,
-              "Merchant raw": result.data.merchant_raw,
+              Date: transaction.date,
+              Time: transaction.time,
+              Amount: transaction.amount,
+              Account: transaction.account,
+              Merchant: transaction.merchant,
+              Category: transaction.category,
+              "Merchant raw": transaction.merchant_raw,
             },
           },
         ],
@@ -207,12 +254,13 @@ app.post("/inbound-email", async (c) => {
   }
 
   await sendNotification(
-    result.data.merchant,
-    result.data.category,
-    result.data.amount,
+    transaction.merchant,
+    transaction.category,
+    transaction.amount,
   );
 
   return c.text("OK", 200);
+  // return c.json(transaction);
 });
 
 Deno.serve(app.fetch);
